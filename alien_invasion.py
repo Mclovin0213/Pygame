907e70736470b6264048a2d17f607b811a2cac5e
Mclovin0213 <javellaneda0213@gmail.com>
2022-05-17 13:52:26 -0700

    Add ability to shoot bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     
@@ -18,12 +19,15 @@ class AlienInvasion:
         pygame.display.set_caption('Alien Invasion') #Sets display caption
         
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         
     def run_game(self):
         while True:
             self._check_events() 
-            self._update_screen()
             self.ship.update()
+            self.bullets.update()
+            self._update_screen()
+
 
     def _check_events(self):
         #Watch for keyboard and mouse events
@@ -42,6 +46,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
             
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -49,9 +55,16 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
             
+    def _fire_bullet(self):
+        # Create a new bullet and add it to the bullets group
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+            
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color) #redrawing screen each pass of loop
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
                     
         pygame.display.flip() #Makes the most recently drawn screen visible
     
