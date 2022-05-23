1e539d112ebe2dd167b518a7c4cbea083aa8e59d
Mclovin0213 <javellaneda0213@gmail.com>
2022-05-23 14:27:30 -0700

    Add score counter, highscore, and level number

--- alien_invasion.py
+++ alien_invasion.py
@@ -71,6 +71,8 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
+            self.sb.prep_level()
             
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
@@ -143,14 +145,20 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
         
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
         
         if not self.aliens:
             # Destroy existing bullets and create new fleet
             self.bullets.empty()
             self._create_fleet()   
             self.settings.increase_speed()
+            
+            # Increase Level.
+            self.stats.level += 1
+            self.sb.prep_level()
     
     def _update_aliens(self):
         """Update the positions of all aliens qqqqin the fleet."""
--- settings.py
+++ settings.py
@@ -19,6 +19,9 @@ class Settings:
         # How quickly the game speeds up
         self.speedup_scale = 1.1
         
+        # How quickly the alien points increase
+        self.score_scale = 1.5
+        
         self.initialize_dynamic_settings()
 
         
@@ -39,4 +42,6 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        
+        self.alien_points = int(self.alien_points * self.score_scale)
         
